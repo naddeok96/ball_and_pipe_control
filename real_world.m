fb83a5d3a786760477dc4bce7f7404fc16e0fe4c
orourk87 <orourk87@students.rowan.edu>
2022-02-08 11:05:40 -0500

    merging braches

--- real_world.m
+++ real_world.m
@@ -10,16 +10,16 @@ close all; clc; clear device;
 
 %% Connect to device
 % device = open serial communication in the proper COM port
-out = serialport(COM4, 19200);
+device = serialport(COM4, 19200);
 
 %% Parameters
 target      = 0.5;   % Desired height of the ball [m]
 sample_rate = 0.25;  % Amount of time between controll actions [s]
 
 %% Give an initial burst to lift ball and keep in air
-% set_pwm(add_proper_args); % Initial burst to pick up ball
+set_pwm(device, 5000); % Initial burst to pick up ball
 pause(0.1) % Wait 0.1 seconds
-% set_pwm(add_proper_args); % Set to lesser value to level out somewhere in
+set_pwm(device, 2000); % Set to lesser value to level out somewhere in
 % the pipe
 
 %% Initialize variables
